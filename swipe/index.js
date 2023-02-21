2cf2dcdcb9ed67da028ba01ecf0c7d719fa98e64
dorianzgraggen <39463388+dorianzgraggen@users.noreply.github.com>
2023-02-21 14:52:32 +0100

    epic moment
    Co-Authored-By: YannicReust <30844126+yannicreust@users.noreply.github.com>

--- swipe/index.js
+++ swipe/index.js
@@ -1,14 +1,23 @@
 const container = document.querySelector(".swipable-container")
 const swipables = document.querySelectorAll(".swipable");
+const slider = document.querySelector(".slider");
+
 console.log(swipables)
 
 container.style.transform = `rotateY(${0}deg) translate3d(${0}px, ${0}px, ${-270}px)`
 
 
-swipables.forEach((swipable, i) => {
-    console.log()
-    // swipable.style.transform = `rotateY(${i * 8}deg)`
+setRotation();
 
-    swipable.style.transform = `rotateY(${i * 60}deg) translate3d(${i * 0}px, ${- i * 0}px, ${270}px)`
+console.log(slider)
+slider.addEventListener("input", e => {
+    console.log(e.target.value)
+    setRotation(e.target.value)
+})
+function setRotation(rotation = 0) {
+    swipables.forEach((swipable, i) => {
+        swipable.style.transform = `rotateY(${rotation * 360 + i * 60}deg) translate3d(${i * 0}px, ${-i * 0}px, ${270}px)`;
 
-});
+    });
+}
+
