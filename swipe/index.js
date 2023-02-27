03e8371ea98122e5727d85fa89c1075d3968d549
dorianzgraggen <39463388+dorianzgraggen@users.noreply.github.com>
2023-02-27 23:43:01 +0100

    so isches

--- swipe/index.js
+++ swipe/index.js
@@ -1,6 +1,8 @@
 const container = document.querySelector(".swipable-container")
 const swipables = document.querySelectorAll(".swipable");
 const slider = document.querySelector(".slider");
+let currentRotation = 0;
+
 
 console.log(swipables)
 
@@ -11,13 +13,30 @@ setRotation();
 
 console.log(slider)
 slider.addEventListener("input", e => {
-    console.log(e.target.value)
     setRotation(e.target.value)
 })
-function setRotation(rotation = 0) {
+
+
+function setRotation(rotation = 0, animate = false) {
     swipables.forEach((swipable, i) => {
-        swipable.style.transform = `rotateY(${rotation * 360 + i * 60}deg) translate3d(${i * 0}px, ${-i * 0}px, ${270}px)`;
+        let degrees = -rotation * 360 + i * 60;
+        const tr = `rotateY(${degrees}deg) translate3d(${i * 0}px, ${-i * 0}px, ${270}px)`;;
+        let transitionDuration = animate ? "1s" : "";
+        swipable.style.transform = tr;
+        swipable.classList.toggle("current", degrees % 360 == 0);
+
+        if (animate) {
+            slider.value = rotation % 1;
+        } else {
+            currentRotation = rotation % 1;
+        }
 
     });
+}
+
+globalThis.handleClick = (multiplier) => {
+    currentRotation += multiplier * (1 / swipables.length);
+    currentRotation = Math.round(currentRotation * 6) / 6;
+    setRotation(currentRotation, true)
 }
 
