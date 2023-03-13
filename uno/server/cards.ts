1fd70fb24aa8598dc990505f3b695cd68c12ca0b
dorianzgraggen <39463388+dorianzgraggen@users.noreply.github.com>
2023-03-13 16:20:09 +0100

    create card class for game logic

---
+++ uno/server/cards.ts
@@ -0,0 +1,41 @@
+const Game = {
+  hasToTake: 0,
+};
+
+type Color = 'none' | 'red' | 'blue' | 'green' | 'yellow';
+
+class Card {
+  color: Color;
+  value: number;
+  flip_direction = false;
+  to_take: number;
+
+  constructor(
+    color: Color,
+    value: number,
+    flip_direction: boolean,
+    to_take: number
+  ) {
+    this.color = color;
+    this.value = value;
+    this.flip_direction = flip_direction;
+    this.to_take = to_take;
+  }
+
+  canPutOn(other: Card): boolean {
+    if (Game.hasToTake > 0) {
+      if (this.color == 'none' && this.to_take > 0) return true;
+      if (this.color == other.color) {
+        if (this.to_take > 0) return true;
+        if (this.flip_direction) return true;
+
+        return false;
+      }
+    }
+
+    if (this.color == 'none') return true;
+    if (this.color == other.color) return true;
+
+    return false;
+  }
+}
